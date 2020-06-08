0c919854358b07da61d439a4ce1ea19180af5e89
Даниил <daniilpost03@gmail.com>
2020-06-08 22:00:38 +0300

    added different currencies

--- app/src/main/java/com/example/convert/MainActivity.kt
+++ app/src/main/java/com/example/convert/MainActivity.kt
@@ -2,18 +2,15 @@ package com.example.convert
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.text.Editable
-import android.text.TextWatcher
-import android.view.View
 import android.widget.ArrayAdapter
 import android.widget.Button
 import android.widget.Spinner
 import android.widget.TextView
 import androidx.core.view.size
-import androidx.core.widget.addTextChangedListener
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
@@ -21,14 +18,15 @@ class MainActivity : AppCompatActivity() {
         val input = findViewById<TextInputEditText>(R.id.input)
         val output = findViewById<TextView>(R.id.output)
         val convert = findViewById<Button>(R.id.convert)
-        val input_name = findViewById<TextView>(R.id.input_name)
-        val output_name = findViewById<TextView>(R.id.output_name)
+        val inputName = findViewById<TextView>(R.id.input_name)
+        val outputName = findViewById<TextView>(R.id.output_name)
         val change = findViewById<Button>(R.id.change)
-        var dollar : Boolean = false
-        var course : Double = 1/74.0
-        val spinner_in = findViewById<Spinner>(R.id.spinner_in)
-        val spinner_out = findViewById<Spinner>(R.id.spinner_out)
-        val change_cur = findViewById<Button>(R.id.change_cur)
+        var course : Double = 1.0
+        val spinnerIn = findViewById<Spinner>(R.id.spinner_in)
+        val spinnerOut = findViewById<Spinner>(R.id.spinner_out)
+        val changeCur = findViewById<Button>(R.id.change_cur)
+        var save : String
+
 
         ArrayAdapter.createFromResource(
             this,
@@ -38,38 +36,32 @@ class MainActivity : AppCompatActivity() {
             // Specify the layout to use when the list of choices appears
             adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
             // Apply the adapter to the spinner
-            spinner_in.adapter = adapter
-            spinner_out.adapter = adapter
+            spinnerIn.adapter = adapter
+            spinnerOut.adapter = adapter
         }
-        change_cur.setOnClickListener {
-            input_name.text = spinner_in.selectedItem.toString()
-            output_name.text = spinner_out.selectedItem.toString()
+
+        var nums_cur = arrayOf("Доллар", "Рубль", "Евро", "Фунт")
+        var curs : MutableMap<String, Array<Double>> = mutableMapOf()
+        curs[nums_cur[0]] = arrayOf(1.0, 74.0, 0.89, 0.79)
+        curs[nums_cur[1]] = arrayOf(0.01, 1.0, 0.01, 87.0)
+        curs[nums_cur[2]] = arrayOf(1.13, 77.0, 1.0, 0.89)
+        curs[nums_cur[3]] = arrayOf(1.27, 87.0, 1.12, 1.0)
+
+
+        changeCur.setOnClickListener {
+            inputName.text = spinnerIn.selectedItem.toString()
+            outputName.text = spinnerOut.selectedItem.toString()
         }
+
         change.setOnClickListener {
-            dollar = !dollar
-            if(dollar)
-            {
-                input_name.text = "Доллар"
-                output_name.text = "Рубль"
-                course = 74.0
-                if(input.text.toString().isNotEmpty()) {
-                    output.text = (input.text.toString().toDouble() * course).toString()
-                    if (output.text.length > 5) output.text = output.text.subSequence(0, 4)
-                }
-            }
-            else
-            {
-                output_name.text = "Доллар"
-                input_name.text = "Рубль"
-                course = 1/74.0
-                if(input.text.toString().isNotEmpty()) {
-                    output.text = (input.text.toString().toDouble() * course).toString()
-                    if (output.text.length > 5) output.text = output.text.subSequence(0, 4)
-                }
-            }
+            save = inputName.text.toString()
+            inputName.text = outputName.text
+            outputName.text = save
         }
+
         convert.setOnClickListener{
             if(input.text.toString().isNotEmpty()) {
+                course = curs[inputName.text.toString()]!![nums_cur.indexOf(outputName.text.toString())]
                 output.text = (input.text.toString().toDouble() * course).toString()
                 if (output.text.length > 5) output.text = output.text.subSequence(0, 4)
             }
